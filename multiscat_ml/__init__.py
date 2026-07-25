02d1a5c9af2c9a6625fcdfb520e67377802f619f
Matthew Ord <matthew.ord1@gmail.com>
2026-07-25 07:21:19 +0000

    linter

--- multiscat_ml/__init__.py
+++ multiscat_ml/__init__.py
@@ -1,6 +1,6 @@
 """An Example project."""
 
-PYTHON_IS_COOL = True  # noqa: RUF067
+PYTHON_IS_COOL = True  # ruff: ignore[non-empty-init-module]
 """A statement about Python."""
 
 __all__ = ["PYTHON_IS_COOL"]
